0a18394eb30591f3b38fbecc100e889261e96662
Marek Metelski <marek@metelski.dev>
2023-09-30 20:56:40 +0200

    Add github_client app and window_to_front linux plugin
    Warning: github_oauth_credentials.dart file is missing!
    
    Add app from desktop app codelab -
    https://codelabs.developers.google.com/codelabs/flutter-github-client#0

---
+++ github_client/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:github/github.dart';
+import 'package:flutter/material.dart';
+import 'package:window_to_front/window_to_front.dart';
+
+import 'github_oauth_credentials.dart';
+import 'src/github_login.dart';
+import 'src/github_summary.dart'; // Add this import
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitHub Client',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'GitHub Client'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GithubLoginWidget(
+      builder: (context, httpClient) {
+        WindowToFront.activate();
+        return FutureBuilder<CurrentUser>(
+          future: viewerDetail(httpClient.credentials.accessToken),
+          builder: (context, snapshot) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+                elevation: 2,
+              ),
+              body: GitHubSummary(
+                  gitHub: _getGitHub(httpClient.credentials.accessToken)),
+            );
+          },
+        );
+      },
+      githubClientId: githubClientId,
+      githubClientSecret: githubClientSecret,
+      githubScopes: githubScopes,
+    );
+  }
+}
+
+Future<CurrentUser> viewerDetail(String accessToken) async {
+  final gitHub = GitHub(auth: Authentication.withToken(accessToken));
+  return gitHub.users.getCurrentUser();
+}
+
+GitHub _getGitHub(String accessToken) {
+  // Modify from here
+  return GitHub(auth: Authentication.withToken(accessToken));
+}                                                                  // to here.
